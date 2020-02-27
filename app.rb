54c42c72e53015b2791a598b50412b5dd5f09190
Daniel Lima <daniel@macluan.local>
2020-02-27 13:07:54 -0300

    Foi adicionado um nv campo mostrando o conhecimento do usuario na area da informatica.

--- app.rb
+++ app.rb
@@ -1,7 +1,9 @@
 require 'sinatra'
 
 get '/' do
-  'Hello world!'
+  @nome = 'hello world!'
+  @idade = '17'
+  erb :sites
 end
 
 get '/luan' do 
@@ -19,4 +21,8 @@ end
 get '/corinthians' do 
   '<p>O Sport Club Corinthians Paulista é um clube multiesportivo brasileiro da cidade de São Paulo, capital do estado homônimo.
     Foi fundado como uma equipe de futebol no dia 1 de setembro de 1910 por um grupo de operários do bairro Bom Retiro.</p>'
+end
+
+get '/Aprediz da progamação' do 
+   '<h1>Franscisco Daniel é um novato na area de informatica</h1>'
 end
