9f4a401697d66240b7ad2876ecc0930b22ec2f42
Daniel Lima <daniel@macluan.local>
2020-02-27 11:00:38 -0300

    Resumo sobre a historia do Corinthians

--- app.rb
+++ app.rb
@@ -15,3 +15,8 @@ end
 get '/Daniel' do
   '<h3> Corithians <h3>'
 end
+
+get '/corinthians' do 
+  '<p>O Sport Club Corinthians Paulista é um clube multiesportivo brasileiro da cidade de São Paulo, capital do estado homônimo.
+    Foi fundado como uma equipe de futebol no dia 1 de setembro de 1910 por um grupo de operários do bairro Bom Retiro.</p>'
+end
